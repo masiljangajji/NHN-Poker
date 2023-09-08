52c1c8aff16b4744390791d3a9751c83de1f46e1
masiljangajji <hjk172262@gmail.com>
2023-09-08 13:17:16 +0900

    first commit

---
+++ src/main/java/Card.java
@@ -0,0 +1,27 @@
+public class Card {
+
+
+    // 52장
+    int number;  // 숫자     ace , 2 3 4 5 6 7 8 9 10  , Joney , Queem , King   , Ace는 1도되고 14도 된다 .
+    String symbol; // 무늬 하트 , 다이아 , 클로버 , 스페이드 ,
+
+    // 각각의 숫자는 4개의 무늬를 가진다
+
+    // 따라서 13*4 = 52장 의 카드 셋을 가지게 된다.
+
+
+    public Card(int number,String symbol) {
+        this.number = number;
+        this.symbol=symbol;
+    }
+
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+}
---
+++ src/main/java/CardSet.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardSet {
+
+
+    List<Card> list= new ArrayList<>();
+
+    // 52장의 카드 셋을 가지고 있는 list
+
+
+    void deleteCardSet(){
+
+    } // 카드 뽑아서 플레이어들 한테 줄꺼임 , 이건 random하게 뽑을꺼고 , 뽑힌 idx는 remove해주면 됨
+
+    void
+
+
+
+
+
+
+}
