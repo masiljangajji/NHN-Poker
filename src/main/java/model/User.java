888626febbe5f80cb8d25b599e85153d89b3f5c3
masiljangajji <hjk172262@gmail.com>
2023-09-08 15:19:32 +0900

    feat: User 카드 add,remove

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,11 +1,23 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class User {
 
+    List<Card>list = new ArrayList<>();
 
+    // add  remove;
+
+
+    void add(Card card){
+        this.list.add(card);
+    }
+
+    void remove(int idx){
+        this.list.remove(idx);
+    }
 
 
 }
