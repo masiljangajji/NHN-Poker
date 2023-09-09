64f69100584a4528f6f08885334fec6c4276562f
masiljangajji <hjk172262@gmail.com>
2023-09-09 14:21:00 +0900

    refactor: (initSet) 초기화 로직 변경

--- src/main/java/model/CardSet.java
+++ src/main/java/model/CardSet.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class CardSet {
@@ -27,18 +29,28 @@ public class CardSet {
         String[] numbers =
                 {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "J", "Q", "K"};
 
+
+        List<Card> list = new ArrayList<>();
+
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < symbols.length; j++) {
-                this.cardQueue.add(new Card(numbers[i], symbols[j]));
+                list.add(new Card(numbers[i],symbols[j]));
             }
         }
 
+        while(!list.isEmpty()){
+            int num=(int)(Math.random()*(list.size()));
+            this.cardQueue.add(list.get(num));
+            list.remove(num);
+        }
+
+
     }
+
     public void giveCard(User user) { // 여기에 타입을 줄까
 
         for (int i = 0; i < 5; i++) {
             user.add(this.cardQueue.poll()); // 플레이어 카드 셋에 추가
-            this.cardQueue.remove(); // 카드 셋에서 삭제
         }
     } // 카드 뽑아서 플레이어들 한테 줄꺼임 , 이건 random하게 뽑을꺼고 , 뽑힌 idx는 remove해주면 됨
 
