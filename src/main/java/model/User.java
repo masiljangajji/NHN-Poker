752fcf6342b3d532552b6c3235855dd567bef7fd
masiljangajji <hjk172262@gmail.com>
2023-09-09 15:48:15 +0900

    feat: (printCardSet) 유저 Card 출력 기능 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,23 +1,36 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class User {
 
-    List<Card>list = new ArrayList<>();
+    private static final Logger logger = LoggerFactory.getLogger(User.class);
 
-    // add  remove;
+    List<Card> list = new ArrayList<>();
 
 
-    void add(Card card){
+    public void add(Card card) {
         this.list.add(card);
     }
 
-    void remove(int idx){
+    public void remove(int idx) {
         this.list.remove(idx);
     }
 
+    public void printCardSet() {
+        Iterator<Card> iterator = list.iterator();
+
+        for (int i = 0; i < list.size(); i++) {
+            logger.info("{} 번째 카드 " + list.get(i), i + 1);
+        }
+    }
+
+    public List<Card> getList() {
+        return list;
+    }
 
 }
